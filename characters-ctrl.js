598ad648be71b13e0fe245aeec3f5b8238ea8a03
Nicole Florence Santos Gonçalves <nicoleflorencesg@gmail.com>
2016-09-19 11:00:45 -0300

    Aplicação AngularJS

---
+++ characters-ctrl.js
@@ -0,0 +1,27 @@
+﻿function CharactersCtrl($scope) {
+    $scope.characters = [
+      { menu: 'home'},
+      { menu: 'about'},
+      { menu: 'services'},
+      { menu: 'portfolio' },
+
+      { menu2: 'team' },
+      { menu2: 'shop' },
+      { menu2: 'blog' },
+      { menu2: 'contact' },
+
+       { title1: 'Latest Project' },
+      { title2: 'Design Projects' },
+      { title3: 'Featured Project' },
+      { title4: 'Protein Jar Packaging' },
+
+       { subtitle1: 'MACKBOOK AIR MOCKUP FREE PSD GRAPHICS' },
+      { subtitle2: 'PSD FILES AND RESOURCES FOR FREE DOWNLOAD' },
+      { subtitle3: 'PROFESSIONAL BUSINESS WEBSITE TEMPLATE FREE PSD' },
+      { subtitle4: 'PROTEIN JAR PACKAGING MOCKUP FREE PSD GRAPHICS' },
+    ];
+}
+
+angular
+  .module('myApp')
+  .controller('CharactersCtrl', CharactersCtrl);
